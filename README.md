c971e70a07105aec255e04b1ac1e76720d9ef7fd
yash-wst <yash_shirbhate@weshinetech.biz>
2023-10-30 11:39:43 +0530

    QPX-2: WIP QPD with print options

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # exam-browser
-
+[![CodeQL](https://github.com/yash-wst/QPD/actions/workflows/codeql.yml/badge.svg)](https://github.com/yash-wst/QPD/actions/workflows/codeql.yml)
 ## usage
 1. Install dependencies: `npm install`
 2. Run the app: `npm run start`
--- src/utility.js
+++ src/utility.js
@@ -1,100 +1,96 @@
-const { Notification, screen, shell } = require('electron');
-const { exec } = require('child_process');
-const { WINDOWS_APPS, LINUX_APPS } = require('./constants');
+const { Notification, screen, shell } = require('electron')
+const { exec } = require('child_process')
+const { WINDOWS_APPS, LINUX_APPS } = require('./constants')
 
-let isRemoteDesktopRunning = false;
+let isRemoteDesktopRunning = false
 
-function Notify(_Title, _Body){
-
-    new Notification({
-        title: _Title,
-        body: _Body,
-        timeoutType: "never",
-        urgency: "critical"
-    }).show();
-
+function Notify (_Title, _Body) {
+  new Notification({
+    title: _Title,
+    body: _Body,
+    timeoutType: 'never',
+    urgency: 'critical'
+  }).show()
 }
 
-function checkMultipleDisplays(){
-  displays = screen.getAllDisplays().length;
-  if (displays === 1){
-    return false;
+function checkMultipleDisplays () {
+  displays = screen.getAllDisplays().length
+  if (displays === 1) {
+    return false
   }
-  return true;
+  return true
 }
 
-function multipleDisplaysAction(){
-  shell.beep();
-  Notify("Critical Alert", "Cannot initiate with multiple displays attached!");
+function multipleDisplaysAction () {
+  shell.beep()
+  Notify('Critical Alert', 'Cannot initiate with multiple displays attached!')
 }
 
-function checkApplications() {
+function checkApplications () {
+  const isWindows = process.platform === 'win32'
+  const isMacOS = process.platform === 'darwin'
+  const isLinux = process.platform === 'linux'
 
-    const isWindows = process.platform === 'win32';
-    const isMacOS = process.platform === 'darwin';
-    const isLinux = process.platform === 'linux';
+  let command
 
-    let command;
+  if (isWindows) {
+    WINDOWS_APPS.find((app) => {
+      command = 'tasklist /FI "IMAGENAME eq ' + app + '*"'
+      exec(command, (error, stdout) => {
+        if (error) {
+          console.error(error.message)
+          return
+        }
+        // If stdout contains any output, a remote desktop application is running
+        isRemoteDesktopRunning = stdout.trim().length > 62
+        if (isRemoteDesktopRunning) {
+          Notify('REMOTE ACCESS ALERT', 'Close all remote access apps!')
+          console.log(app + ' is running.')
+          isRemoteDesktopRunning = true
+          return isRemoteDesktopRunning
+          // Perform actions if a remote desktop application is detected
+        } else {
+          console.log('No remote desktop application is running.')
+          isRemoteDesktopRunning = false
+          // Perform actions if no remote desktop application is detected
+        }
+      })
+    })
+  } else if (isMacOS) {
+    command = 'pgrep ' + a + '; echo $?'
+  } else if (isLinux) {
+    LINUX_APPS.find((app) => {
+      command = 'pgrep ' + app + '; echo $?'
+      exec(command, (error, stdout) => {
+        if (error) {
+          console.error(error.message)
+          return
+        }
 
-    if (isWindows) {
-      WINDOWS_APPS.find((app)=> {
-        command = 'tasklist /FI "IMAGENAME eq '+app+'*"';
-        exec(command, (error, stdout) => {
-          if (error) {
-            console.error(error.message);
-            return;
-          }
-          // If stdout contains any output, a remote desktop application is running
-          isRemoteDesktopRunning = stdout.trim().length > 62
-          if (isRemoteDesktopRunning) {
-            Notify("REMOTE ACCESS ALERT", "Close all remote access apps!");
-            console.log(app+' is running.');
-            isRemoteDesktopRunning = true;
-            return isRemoteDesktopRunning;
-            // Perform actions if a remote desktop application is detected
-          } else {
-            console.log('No remote desktop application is running.');
-            isRemoteDesktopRunning = false;
-            // Perform actions if no remote desktop application is detected
-          }
-        });
-      });
-    } else if (isMacOS) {
-      command = 'pgrep '+a+"; echo $?";
-    } else if (isLinux) {
-      LINUX_APPS.find((app) => {
-        command = 'pgrep '+app+"; echo $?";
-        exec(command, (error, stdout) => {
-          if (error) {
-            console.error(error.message);
-            return;
-          }
+        let isRemoteDesktopRunning = false
+        // If stdout contains any output, a remote desktop application is running
+        isRemoteDesktopRunning = stdout.trim().length > 1
 
-          let isRemoteDesktopRunning = false;
-          // If stdout contains any output, a remote desktop application is running
-          isRemoteDesktopRunning = stdout.trim().length > 1
-
-          if (isRemoteDesktopRunning) {
-            Notify("REMOTE ACCESS ALERT", "Close all remote access apps!");
-            console.log('A remote desktop application is running on Linux.');
-            isRemoteDesktopRunning = true;
-            return isRemoteDesktopRunning;
-            // Perform actions if a remote desktop application is detected
-          } else {
-            console.log('No remote desktop application is running on Linux.');
-            isRemoteDesktopRunning = false;
-            // Perform actions if no remote desktop application is detected
-          }
-        });
-      });
-    }
-    return isRemoteDesktopRunning;
+        if (isRemoteDesktopRunning) {
+          Notify('REMOTE ACCESS ALERT', 'Close all remote access apps!')
+          console.log('A remote desktop application is running on Linux.')
+          isRemoteDesktopRunning = true
+          return isRemoteDesktopRunning
+          // Perform actions if a remote desktop application is detected
+        } else {
+          console.log('No remote desktop application is running on Linux.')
+          isRemoteDesktopRunning = false
+          // Perform actions if no remote desktop application is detected
+        }
+      })
+    })
+  }
+  return isRemoteDesktopRunning
 }
-
 
 module.exports = {
-    Notify,
-    checkApplications,
-    checkMultipleDisplays,
-    multipleDisplaysAction
+  Notify,
+  checkApplications,
+  checkMultipleDisplays,
+  multipleDisplaysAction
 }
